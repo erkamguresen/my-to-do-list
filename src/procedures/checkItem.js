eae6ed7c16099df34ad4210177290daf89405a50
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-06-17 11:21:34 +0200

    strike thorugh and ceheck are working

--- src/procedures/checkItem.js
+++ src/procedures/checkItem.js
@@ -2,7 +2,21 @@ import { getLIItem } from '../logic/getLiItem.js';
 
 export function checkItem(event) {
   const liElement = getLIItem(event);
-  //TODO check item box state
-  console.log('check function');
-  console.log(liElement);
+
+  const checkBox = liElement.querySelector('.check');
+
+  const spanElement = liElement.querySelector('span');
+
+  const classList = checkBox.classList;
+
+  if (
+    classList.contains('fa-square') &&
+    !classList.contains('fa-check-square')
+  ) {
+    classList.remove('fa-square');
+    classList.add('fa-check-square');
+  } else if (classList.contains('fa-check-square')) {
+    classList.remove('fa-check-square');
+    classList.add('fa-square');
+  }
 }
