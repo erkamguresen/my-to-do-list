df7042824d34f6a42b5ab5674a674d7786505f12
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-07-08 15:03:11 +0200

    ul css bug fixed

--- src/components/ulElement.js
+++ src/components/ulElement.js
@@ -2,6 +2,7 @@ import { createLiElement } from './liElement.js';
 
 export function createUlElement(listArray) {
   const ulElement = document.createElement('ul');
+  ulElement.className = 'list-group';
 
   listArray.forEach((listItem) => {
     const liElement = createLiElement(listItem);
