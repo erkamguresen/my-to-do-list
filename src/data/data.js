c7b2e9749512489e7ef14fec644a316931ce63dc
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-07-08 19:22:25 +0200

    example data

--- src/data/data.js
+++ src/data/data.js
@@ -7,11 +7,73 @@ import { getMyToDoListDataSet } from '../IO/IO-LocalStorage.js';
  * This is for all ToDo lists. It gets from local storage
  * or creates a new empty one
  */
-export const myToDoListDataBase = {
-  myToDoLists: getMyToDoListDataSet() || [],
+const toDoListData = {
+  toDoLists: [
+    // list 1
+    {
+      listId: 1625764536952,
+      toDos: [
+        // list item 1
+        {
+          itemId: 1625764684884,
+          itemName: 'First thing to do',
+          isChecked: false,
+        },
+        {
+          itemId: 1625764684894,
+          itemName: 'Second thing to do',
+          isChecked: true,
+        },
+        {
+          itemId: 1625764684895,
+          itemName: 'Third thing to do',
+          isChecked: false,
+        },
+      ],
+    },
+    // list 2
+    {
+      listId: 1625764551531,
+      toDos: [
+        {
+          itemId: 1625764684184,
+          itemName: 'List Item 1',
+          isChecked: true,
+        },
+        {
+          itemId: 1625764684294,
+          itemName: 'List Item 2',
+          isChecked: true,
+        },
+        {
+          itemId: 1625764684395,
+          itemName: 'List Item 3',
+          isChecked: false,
+        },
+      ],
+    },
+
+    {
+      listId: 1625764551533,
+      toDos: [
+        {
+          itemId: 1625764684584,
+          itemName: 'Not Checked Item 1',
+          isChecked: false,
+        },
+        {
+          itemId: 1625764684694,
+          itemName: 'Not Checked Item 2',
+          isChecked: false,
+        },
+        {
+          itemId: 1625764684795,
+          itemName: 'Not Checked Item 3',
+          isChecked: false,
+        },
+      ],
+    },
+  ],
 };
 
-export const currentData = {
-  currentToDoListName: '',
-  currentToDoList: ['o', 'bu', 'su', 'etc'],
-};
+export { toDoListData as state };
