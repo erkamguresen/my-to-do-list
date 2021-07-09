e54a771aafc54f0bf85d52bae06168413df57244
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-07-09 17:52:56 +0200

    form listeners are added

--- src/components/Pages/Home/homeIndex.js
+++ src/components/Pages/Home/homeIndex.js
@@ -1,4 +1,6 @@
 import { state } from '../../../data/data.js';
+import { addNewListHandler } from '../../../handlers/addNewListHandler.js';
+import { filterListsHandler } from '../../../handlers/filterListsHandler.js';
 import { getDataSet } from '../../../IO/IO-LocalStorage.js';
 import { createFormPanel } from '../../shared/formPanel.js';
 
@@ -8,5 +10,17 @@ state.toDoLists = getDataSet();
 
 console.log(state);
 
+// the add and search form part
 const mainPanel = document.getElementById('main-panel');
 mainPanel.appendChild(createFormPanel());
+
+// add listeners to the form
+document
+  .getElementById('addForm')
+  .addEventListener('submit', addNewListHandler);
+
+document
+  .getElementById('addItem')
+  .addEventListener('input', filterListsHandler);
+
+// render the list of lists
---
+++ src/handlers/filterListsHandler.js
@@ -0,0 +1,3 @@
+export function filterListsHandler(event) {
+  //TODO: handle
+}
