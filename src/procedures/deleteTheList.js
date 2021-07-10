3f40aac84f7982d89cffdf05f7c96366bde217ab
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-07-10 10:14:06 +0200

    delete list documentation

--- src/procedures/deleteTheList.js
+++ src/procedures/deleteTheList.js
@@ -2,10 +2,13 @@ import { state } from '../data/data.js';
 import { saveDataSet } from '../IO/IO-LocalStorage.js';
 import { renderTheListOfLists } from './renderTheListOfLists.js';
 
+/**
+ * This function deletes the list for the given id.
+ * And saves the new data. Then renders the list of the lists again.
+ *
+ * @param {string} listID - id of the list to delete
+ */
 export function deleteTheList(listID) {
-  console.log(`delete ${listID}`);
-  console.log(typeof listID);
-
   state.toDoLists = state.toDoLists.filter(
     (list) => list.listId !== parseInt(listID)
   );
--- src/procedures/openTheList.js
+++ src/procedures/openTheList.js
@@ -1,3 +1,15 @@
-export function opeTheList(listId) {
+export function opeTheList(listID) {
   // TODO: open then list
+  console.log(listID);
+
+  const mainPanel = document.getElementById('main-panel');
+
+  // clear the container
+  while (mainPanel.firstChild) {
+    mainPanel.removeChild(mainPanel.firstChild);
+  }
+
+  // append the form area
+
+  // render the list
 }
