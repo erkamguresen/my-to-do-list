ce0a4e6991f6f294ef8ecdfa9bd25b9f8033ef57
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-07-10 08:26:58 +0200

    delete function is compleated

--- src/procedures/deleteTheList.js
+++ src/procedures/deleteTheList.js
@@ -1,4 +1,16 @@
+import { state } from '../data/data.js';
+import { saveDataSet } from '../IO/IO-LocalStorage.js';
+import { renderTheListOfLists } from './renderTheListOfLists.js';
+
 export function deleteTheList(listID) {
-  // TODO: delete the list
   console.log(`delete ${listID}`);
+  console.log(typeof listID);
+
+  state.toDoLists = state.toDoLists.filter(
+    (list) => list.listId !== parseInt(listID)
+  );
+
+  saveDataSet();
+
+  renderTheListOfLists();
 }
