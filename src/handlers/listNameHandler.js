270e94e1d9d32e6ef9d91fbe87e35e92ef323dc4
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-07-10 18:38:09 +0200

    simdilik bitti

--- src/handlers/listNameHandler.js
+++ src/handlers/listNameHandler.js
@@ -1,16 +1,21 @@
+import { state } from '../data/data.js';
+import { saveDataSet } from '../IO/IO-LocalStorage.js';
+
 export function listNameEditHandler(event) {
   event.preventDefault();
   event.stopPropagation();
 
-  const headerElement = event.target;
+  if (state.currentToDoListIndex >= 0) {
+    const headerElement = event.target;
 
-  let isEditable = headerElement.getAttribute('contentEditable') === 'true';
+    let isEditable = headerElement.getAttribute('contentEditable') === 'true';
 
-  isEditable = !isEditable;
+    isEditable = !isEditable;
 
-  headerElement.setAttribute('contentEditable', isEditable);
+    headerElement.setAttribute('contentEditable', isEditable);
 
-  headerElement.focus();
+    headerElement.focus();
+  }
 }
 
 export function editFocusOutHandler(event) {
@@ -20,4 +25,11 @@ export function editFocusOutHandler(event) {
   const headerElement = event.target;
 
   headerElement.setAttribute('contentEditable', 'false');
+
+  // update the data
+  state.toDoLists[state.currentToDoListIndex].listName =
+    headerElement.innerText;
+
+  // save the data
+  saveDataSet();
 }
--- src/init/index.js
+++ src/init/index.js
@@ -1,13 +1,7 @@
 import '../components/Pages/Home/homeIndex.js';
 
 import '../listeners/homePageListener.js';
-
-// import '../listeners/listListeners.js';
-// import '../listeners/listNameListener.js';
-// import { renderToDoList } from '../procedures/renderList.js';
+import '../listeners/listNameListener.js';
 
 // import { setMyToDoListDataSet } from './exampledataToLocalStorage.js';
-
-// renderToDoList();
-
 // setMyToDoListDataSet();
