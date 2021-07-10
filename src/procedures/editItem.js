e0d6fa51be1fc6fa97fb607410398acdef92edab
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-07-10 18:12:27 +0200

    edit item compleated

--- src/procedures/editItem.js
+++ src/procedures/editItem.js
@@ -1,9 +1,8 @@
+import { state } from '../data/data.js';
 import { getLIItem } from '../logic/getLiItem.js';
 
 export function editItem(event) {
   const liElement = getLIItem(event);
-
-  console.log('edit enabled');
 
   const spanElement = liElement.querySelector('span');
 
@@ -19,10 +18,29 @@ export function editItem(event) {
 export function disableEditing(event) {
   const targetElement = event.target;
 
-  console.log('edit disabled');
   while (targetElement.nodeName !== 'SPAN') {
     targetElement = targetElement.parentElement;
   }
 
   targetElement.setAttribute('contentEditable', 'false');
+
+  // get the id of the item
+  const toDoID = parseInt(targetElement.parentElement.dataset.id);
+
+  // get the index of the item
+  const toDos = state.toDoLists[state.currentToDoListIndex].toDos;
+  const index = toDos.map((toDo) => toDo.itemId).indexOf(toDoID);
+
+  //update data
+  toDos[index].itemName = targetElement.innerText;
+
+  //save data
+  saveDataSet();
+
+  // render list data
+  const listID = state.toDoLists[state.currentToDoListIndex].listId;
+  renderToDoList(listID.toString());
+
+  // sort list
+  sortList();
 }
