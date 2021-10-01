280ba6fc6fd1c4a5d772104b054e656ce570975b
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-10-01 11:37:09 +0200

    example data bug fix

--- src/init/index.js
+++ src/init/index.js
@@ -1,8 +1,8 @@
-import '../components/Pages/Home/homeIndex.js';
-
 import '../listeners/homePageListener.js';
 import '../listeners/listNameListener.js';
 
 import { setMyToDoListDataSet } from './exampledataToLocalStorage.js';
 
 setMyToDoListDataSet();
+
+import '../components/Pages/Home/homeIndex.js';
