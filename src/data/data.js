47b724ebc93db36f4245483c88aea7462130a10d
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-10-01 11:33:10 +0200

    example data bug fix

--- src/data/data.js
+++ src/data/data.js
@@ -11,62 +11,64 @@ const toDoListData = {
 };
 
 const exampleListData = [
-  // list 1
-  {
-    listName: 'Shopping List (example)',
-    listId: 1625764536952,
-    toDos: [
-      // list item 1
-      {
-        itemId: 1625764684884,
-        itemName: 'Milk',
-        isChecked: false,
-      },
-      {
-        itemId: 1625764684894,
-        itemName: 'Eggs x12',
-        isChecked: true,
-      },
-      {
-        itemId: 1625764684895,
-        itemName: 'Appels',
-        isChecked: false,
-      },
-    ],
-  },
-  // list 2
-  {
-    listName: 'Daily List (example)',
-    listId: 1625764551531,
-    toDos: [
-      {
-        itemId: 1625764684184,
-        itemName: 'Watch the online tutorial',
-        isChecked: true,
-      },
-      {
-        itemId: 1625764684294,
-        itemName: 'Finish the project',
-        isChecked: true,
-      },
-      {
-        itemId: 1625764684395,
-        itemName: 'New episode of my favorite series',
-        isChecked: false,
-      },
-      {
-        itemId: 1625764684396,
-        itemName: 'New episode of my favorite series',
-        isChecked: false,
-      },
-    ],
-  },
+  [
+    // list 1
+    {
+      listName: 'Shopping List (example)',
+      listId: 1625764536952,
+      toDos: [
+        // list item 1
+        {
+          itemId: 1625764684884,
+          itemName: 'Milk',
+          isChecked: false,
+        },
+        {
+          itemId: 1625764684894,
+          itemName: 'Eggs x12',
+          isChecked: true,
+        },
+        {
+          itemId: 1625764684895,
+          itemName: 'Appels',
+          isChecked: false,
+        },
+      ],
+    },
+    // list 2
+    {
+      listName: 'Daily List (example)',
+      listId: 1625764551531,
+      toDos: [
+        {
+          itemId: 1625764684184,
+          itemName: 'Watch the online tutorial',
+          isChecked: true,
+        },
+        {
+          itemId: 1625764684294,
+          itemName: 'Finish the project',
+          isChecked: true,
+        },
+        {
+          itemId: 1625764684395,
+          itemName: 'New episode of my favorite series',
+          isChecked: false,
+        },
+        {
+          itemId: 1625764684396,
+          itemName: 'New episode of my favorite series',
+          isChecked: false,
+        },
+      ],
+    },
 
-  {
-    listName: 'An new empty List (example)',
-    listId: 1625764551533,
-    toDos: [],
-  },
+    {
+      listName: 'An new empty List (example)',
+      listId: 1625764551533,
+      toDos: [],
+    },
+  ],
 ];
 
 const toDoListExampleData = {
